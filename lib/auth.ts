9eaba6b7ab0c24542647343e32dd3fb3b2b47f6f
Sameer Basnet <basnetsameer78@gmail.com>
2025-02-05 23:29:02 +0545

    used auth

---
+++ lib/auth.ts
@@ -0,0 +1,64 @@
+import bcrypt from "bcryptjs";
+import { error } from "console";
+import CredentialsProvider from "./../node_modules/next-auth/src/providers/credentials";
+
+import { NextAuthOptions } from "next-auth";
+import { connectToDatabase } from "./db";
+import User from "@/models/User";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw error("Email and password are required");
+        }
+
+        try {
+          await connectToDatabase();
+          const user = await User.findOne({ email: credentials.email });
+
+          if (!user) {
+            throw error("No user found");
+          }
+
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (!isValid) {
+            throw error("Invalid password");
+          }
+          return { id: user._id.toString(), email: user.email };
+        } catch (error) {
+          console.log(error);
+          throw new Error("Failed to authenticate");
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+};
