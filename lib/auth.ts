a068b5bf1e3f6a75b3e551a3392ee9b6758347e2
Sameer Basnet <basnetsameer78@gmail.com>
2025-02-10 17:53:37 +0545

    completed auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -58,7 +58,13 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
   },
+  pages:{
+    signIn:'/login',
+    error:'/login',
+  },
   session: {
     strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
   },
+  secret: process.env.NEXTAUTH_SECRET,
 };
